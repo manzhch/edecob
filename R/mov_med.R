5c1eda8af99883c2515b44c4d37167b26619d6e9
Man, Zheng Chen {MDBL~Basel} <zheng_chen.man@roche.com>
2021-07-19 09:08:25 +0200

    merged mov_med.R and mov_med_resid.R

--- R/mov_med.R
+++ R/mov_med.R
@@ -88,3 +88,15 @@ mov_med <- function(data,
   )
   med_pts <- med_pts[!is.na(med_pts$date), ]
 }
+
+mov_med_resid <- function(data, date, med_pts) {
+
+  resid <- numeric(length(data))
+
+  if (nrow(data) > 0 && nrow(med_pts) > 0) {
+    for (ii in 1:nrow(med_pts)) {
+      dataset_ind <- as.logical(date == med_pts$date[ii])
+      resid[dataset_ind] <- data[dataset_ind] - med_pts$med[ii]
+    }
+  }
+}
