f89191df250e545689e5c5bdc5e20b5578b0e71c
Man, Zheng Chen {MDBL~Basel} <zheng_chen.man@roche.com>
2021-09-15 13:54:33 +0200

    example data

---
+++ R/example_data.R
@@ -0,0 +1,16 @@
+#' Artificially generated data for examples
+#'
+#' A dataset in the long format containing the jump height
+#' of 5 subjects over 350 days.
+#'
+#' @format A dataframe with 2100 rows and 5 variables:
+#' \describe{
+#'   \item{subject}{The subject identifier}
+#'   \item{study_day}{The study day from which the data is collected}
+#'   \item{jump_height}{The jump height, in cm}
+#'   \item{baseline}{The baseline for the subject}
+#'   \item{threshold}{The threshold for the subject}
+#' }
+#'
+#'
+"example_data"
